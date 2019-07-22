76f5346a8f7f1acb0014b44b30235e963bb5a6f5
akolvakh <a.kolvakh@outlook.com>
2019-07-23 00:32:52 +0300

    last

--- sources/system.c
+++ sources/system.c
@@ -14,7 +14,7 @@ void	sys_validator(char *str, t_dataset *data)
 					== 0)) && (!(ft_strcmp(str, "tricorn") == 0))
 			&& (!(ft_strcmp(str, "juliacubed") == 0)) &&
 			(!(ft_strcmp(str, "mandelcubed") == 0)))
-		sys_error(VALUE);
+		sys_error(USAGE);
 	if (ft_strcmp(str, "julia") == 0)
 		data->fractol = 1;
 	if (ft_strcmp(str, "mandel") == 0)
@@ -42,7 +42,13 @@ void	sys_error(int error)
 	|| (error == FILE && (msg = "ERROR: can't open file\n"))
 	|| (error == MALLOC && (msg = "ERROR: can't malloc memory\n"))
 	|| (error == INPUT && (msg = "ERROR: too much input\n"))
-	|| (error == FINISH && (msg = "MESSAGE: Exit the program\n")))
+	|| (error == FINISH && (msg = "MESSAGE: Exit the program\n"))
+	|| (error == USAGE && (msg = "Fractols:\n1. julia\n2. mandel\n\
+				3. ship\n4. carpet\n5. tricorn\n6. juliacubed \n7. mandelcubed\n\
+				Controls:\nMove: Arrow Keys\nZoom: Page Up & Page Down\n\
+				Mouse Zoom: Scrool Wheel\nIteration: Left Click & (+ -) Keys\n\
+				Zoom Reset: Space Bar\nJulia Activate: Right Click\n\
+				Color Change: Num Key 1-3\nHue Change: Num Key 4 & 5")))
 		ft_putstr(msg);
 	exit(-1);
 }
@@ -51,7 +57,12 @@ void	sys_message(int message)
 {
 	char *msg;
 
-	if ((message == USAGE && (msg = "Fractols:\n1. julia\n2. mandel\n3. ship\n4. carpet\n5. tricorn\n6. juliacubed \n7. mandelcubed\nControls:\nMove: Arrow Keys\nZoom: Page Up & Page Down\nMouse Zoom: Scrool Wheel\nIteration: Left Click & (+ -) Keys\nZoom Reset: Space Bar\nJulia Activate: Right Click\nColor Change: Num Key 1-3\nHue Change: Num Key 4 & 5"))
+	if ((message == USAGE && (msg = "Fractols:\n1. julia\n2. mandel\n\
+				3. ship\n4. carpet\n5. tricorn\n6. juliacubed \n7. mandelcubed\n\
+				Controls:\nMove: Arrow Keys\nZoom: Page Up & Page Down\n\
+				Mouse Zoom: Scrool Wheel\nIteration: Left Click & (+ -) Keys\n\
+				Zoom Reset: Space Bar\nJulia Activate: Right Click\n\
+				Color Change: Num Key 1-3\nHue Change: Num Key 4 & 5"))
 	|| (message == START && (msg = "MESSAGE: Start the game\n"))
 	|| (message == LEFT && (msg = "MESSAGE: Left\n"))
 	|| (message == RIGHT && (msg = "MESSAGE: Right\n"))
